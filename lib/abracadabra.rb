c9d3c5f8db124b3eca4f818216c6cb1bc83fe211
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-03-26 17:54:22 -0500

    Fixing rails issues

--- lib/abracadabra.rb
+++ lib/abracadabra.rb
@@ -1,4 +1,1 @@
-require "abracadabra/view_helpers"
 require "abracadabra/engine"
-
-ActionView::Base.send(:include, ViewHelpers) if defined?(ActionView::Base)
--- lib/abracadabra/engine.rb
+++ lib/abracadabra/engine.rb
@@ -1,4 +1,9 @@
 module Abracadabra
   class Engine < Rails::Engine
+    initialize 'abracadabra.initialize' do
+      ActiveSupport.on_load(:action_view) do
+        include Abracadabra::ViewHelpers
+      end
+    end
   end
 end
