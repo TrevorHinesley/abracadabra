67057e7632f75885b9bfe0208cd274205d55b82e
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-03-26 09:32:51 -0500

    Fixing view helpers

--- lib/abracadabra.rb
+++ lib/abracadabra.rb
@@ -1,11 +1,11 @@
 require "abracadabra/version"
-require 'abracadabra/view_helper'
+require 'abracadabra/view_helpers'
 
 module Abracadabra
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'abracadabra.view_helper' do
-        ActionView::Base.send :include, ViewHelper
+      initializer 'abracadabra.view_helpers' do
+        ActionView::Base.send :include, ViewHelpers
       end
     end
   end
