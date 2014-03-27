7e7b24c6e26f4c01b81edf754b6985e2f3159f07
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-03-27 10:04:56 -0500

    Added remote true/false and data type. Also swapped raw HTML for link_to tag to keep with convention.

--- app/helpers/abracadabra/rails/view_helper.rb
+++ app/helpers/abracadabra/rails/view_helper.rb
@@ -6,7 +6,10 @@ module Abracadabra
         link_class = "#{options[:class]} abracadabra".strip
         value = options[:value] || instance.send(options[:attribute])
         method = options[:method] || "patch"
-        "<a href='javascript:void(0)' class='#{link_class}' data-path='#{options[:path]}' data-method='#{method}' data-attribute='#{options[:attribute]}' data-class='#{instance_class}'>#{value}</a>".html_safe
+        remote? = options[:remote].nil? || options[:remote] == true ? true : false
+        data_type = options[:type] || "script"
+
+        link_to value, "javascript:void(0)", class: link_class, method: method.to_sym, data: { path: options[:path], attribute: options[:attribute], class: instance_class, type: data_type.to_sym }, remote: remote?
       end
     end
   end
