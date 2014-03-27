1af58d991a51c682e0fb3fbe7a9a9b13a2618902
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-03-27 09:42:25 -0500

    V1 released, README updated.

---
+++ app/helpers/abracadabra/rails/view_helper.rb
@@ -0,0 +1,13 @@
+module Abracadabra
+  module Rails
+    module ViewHelper
+      def click_to_edit(instance, options)
+        instance_class = instance.class.to_s.underscore
+        link_class = "#{options[:class]} abracadabra".strip
+        value = options[:value] || instance.send(options[:attribute])
+        method = options[:method] || "patch"
+        "<a href='javascript:void(0)' class='#{link_class}' data-path='#{options[:path]}' data-method='#{method}' data-attribute='#{options[:attribute]}' data-class='#{instance_class}'>#{value}</a>".html_safe
+      end
+    end
+  end
+end
