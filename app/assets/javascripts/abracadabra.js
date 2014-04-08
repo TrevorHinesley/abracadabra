9bc379d108218785b822cd1b50bd04159d3d5745
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-04-08 16:59:07 -0500

    Further simplified JS

--- app/assets/javascripts/abracadabra.js
+++ app/assets/javascripts/abracadabra.js
@@ -6,26 +6,23 @@ $(function() {
     } else {
       container = $element.parents(".abracadabra-container");
     }
-    value = container.find(".abracadabra-input-container input").val();
+    value = container.find(".abracadabra-input").val();
     container.siblings(".abracadabra").text(value).show();
     container.remove();
   }
 
   $("body").on("ajax:success", ".abracadabra-form", function(e) {
-    console.log("ajax success");
     closeAbracadabra(this);
   })
 
   $("body").on("click", ".abracadabra-cancel", function() {
-    console.log("cancel click");
     closeAbracadabra(this);
   });
 
-  $("body").on("keyup", ".abracadabra-input-container input", function(e) {
+  $("body").on("keydown", ".abracadabra-input", function(e) {
     // Press Tab to submit (same function as Enter key)
     if (e.keyCode == 9)
     {
-      console.log("tab pressed");
       e.preventDefault();
       $(this.form).submit();
     }
@@ -62,7 +59,7 @@ $(function() {
     buttons = "<button type=\"submit\" class=\"btn btn-primary abracadabra-submit\"><i class=\"fa fa-check\"></i></button><button type=\"button\" class=\"btn abracadabra-cancel\"><i class=\"fa fa-times\"></i></button>";
     openFormTag = "<form accept-charset=\"UTF-8\" action=\"" + path + "\"" + remote + type + " class=\"form-inline abracadabra-form\" method=\"post\">";
     hiddenMethodTags = "<div style=\"display:none;\"><input name=\"utf8\" type=\"hidden\" value=\"&#10003;\"><input name=\"_method\" type=\"hidden\" value=\"" + formMethod + "\">" + authToken + "</div>";
-    input = "<input type=\"text\" class=\"form-control\" id=\"" + inputId + "\" name=\"" + inputName + "\" value=\"" + inputValue + "\">";
+    input = "<input type=\"text\" class=\"form-control abracadabra-input\" id=\"" + inputId + "\" name=\"" + inputName + "\" value=\"" + inputValue + "\">";
     
     html = "<span class=\"abracadabra-container abracadabra-inline\">" + openFormTag + hiddenMethodTags;
     html += "<div class=\"control-group\"><div class=\"abracadabra-input-and-button-wrapper\"><div class=\"abracadabra-input-container\">" + input + "</div>";
